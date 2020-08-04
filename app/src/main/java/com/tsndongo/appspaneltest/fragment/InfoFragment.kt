63b5ca147d3958a6d8dbec03910274e008de3d31
Takou Syntiche N'Dongo <tsndongo@gmail.com>
2020-08-04 12:54:33 +0200

    first commit

---
+++ app/src/main/java/com/tsndongo/appspaneltest/fragment/InfoFragment.kt
@@ -0,0 +1,20 @@
+package com.tsndongo.appspaneltest.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.tsndongo.appspaneltest.R
+
+class InfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_actuality, container, false)
+    }
+}
---
+++ app/src/main/java/com/tsndongo/appspaneltest/model/Actuality.kt
@@ -0,0 +1,10 @@
+package com.tsndongo.appspaneltest.model
+
+import java.util.Date
+
+data class Actuality(
+    val image: String,
+    val date: Date,
+    val title: String,
+    val description: String
+)
